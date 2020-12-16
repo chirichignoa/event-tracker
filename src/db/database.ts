e5f1f12979ef7fbbfbe5440a8ab931e9d33f8bc9
Agustin Chirichigno <chirichignoa@gmail.com>
2020-12-15 22:41:59 -0300

    Add environment config for database

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,11 +1,30 @@
-import {Connection, createConnection} from "typeorm";
+import {Connection, ConnectionOptions, createConnection} from "typeorm";
 import { logger } from '../util/logger';
+import dotenv from 'dotenv';
+dotenv.config();
 
 let connection: Connection;
 
+const dbConfig: ConnectionOptions = {
+  "type": "mysql",
+  "host": process.env.DB_HOST || 'localhost',
+  "port": Number(process.env.DB_PORT) || 3306,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASS,
+  "database": process.env.DB_NAME || "event_tracker",
+  "synchronize": true,
+  "logging": false,
+  "entities": [
+     "src/model/entities/**/*.ts"
+  ],
+  "insecureAuth": true
+}
+
+console.log(dbConfig);
+
 export async function connect() {
   logger.info('Trying to connect to database');
-  connection = await createConnection(); 
+  connection = await createConnection(dbConfig); 
 }
 
 export  { connection };
